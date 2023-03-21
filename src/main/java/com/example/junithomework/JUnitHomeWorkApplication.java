28ba4e04f11a7cf3de862ec80bc7a7cec4a46e6d
VladimirSherban <Vovan_Sh_94@mail.ru>
2023-03-21 04:36:00 +0300

    First commit!

---
+++ src/main/java/com/example/junithomework/JUnitHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.junithomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnitHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JUnitHomeWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/junithomework/JUnitHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junithomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitHomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
